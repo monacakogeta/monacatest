c4bd79588a3a0318b3e2f85f82f91d5590d27928
monacakogeta <monaca.kurom.tablet@gmail.com>
2018-04-03 15:17:03 +0900

    Project's Initial Commit

---
+++ www/js/app.js
@@ -0,0 +1,14 @@
+// This is a JavaScript file
+
+var aaKey = ""
+var clientKey = ""
+
+// SDKの初期化
+var ncmb = new NCMB(aaKey, clientKey)
+
+var currentLoginUser
+
+
+function onIDregisterBtn() {
+  var userName = $('#reg_username').val();
+}
